7455b61107434cc7f72264f64fc494d6baafbde5
Piyush Katole <katolepiyush16@gmail.com>
2025-08-19 11:16:28 +0530

    fix: Update app version to 2.0.5 and correct API_URL in config

--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
 // #  NEXT_PUBLIC_API_URL=https://api.karyah.in
 //  NEXT_PUBLIC_API_URL=http://192.168.1.137:5001
-// export const API_URL = 'https://api.karyah.in/';
-export const API_URL = 'http://localhost:5001/';
+export const API_URL = 'https://api.karyah.in/';
+// export const API_URL = 'http://localhost:5001/';
 // export const API_URL = 'http://192.168.1.137:5000/';
